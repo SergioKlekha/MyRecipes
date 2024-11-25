1868aaba87f67d6f4f5f7b537f0167e42bda9545
RedDante <92797345+RedDante@users.noreply.github.com>
2024-11-25 12:16:38 +0000

    homeMade

---
+++ src/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+
+
+const Profile = () => {
+    return (
+        <div>
+            <img src="" alt="" />
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    );
+};
+
+export default Profile;
